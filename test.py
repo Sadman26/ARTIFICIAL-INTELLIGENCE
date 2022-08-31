75545ed1114d370091baac13131a4ca825e17e69
Sadman-123 <19201103123@cse.bubt.edu.bd>
2022-08-31 12:19:45 +0600

    initial

---
+++ test.py
@@ -0,0 +1,10 @@
+print('Hello World')#print a message
+x=123#var declare
+l1,l2,l3=100,200,300#multiple var declare
+z=str(x)#type casting
+array=[231,12,34,45,65,77,886]
+print("LENGHT: ",len(array))
+print("First element is ",array[0])
+print("Your ID is",x)
+print('It is now ',type(z)) 
+print(l1,l2,l3)
