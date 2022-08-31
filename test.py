b797c585b45a4b0165227b8967b3f51c8004778b
Sadman-123 <19201103123@cse.bubt.edu.bd>
2022-08-31 12:23:15 +0600

    added range print in loop

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ l1,l2,l3=100,200,300#multiple var declare
 z=str(x)#type casting
 array=[231,12,34,45,65,77,886]
 print("LENGHT: ",len(array))
-print("First element is ",array[0])
+print(" elements is ",array[0:2])
 print("Your ID is",x)
 print('It is now ',type(z)) 
 print(l1,l2,l3)
