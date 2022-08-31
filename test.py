37dd22f265ec095fe44988bbf609ccbab388aec2
Sadman-123 <19201103123@cse.bubt.edu.bd>
2022-08-31 12:25:23 +0600

    change starting index

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ print('Hello World')#print a message
 x=123#var declare
 l1,l2,l3=100,200,300#multiple var declare
 z=str(x)#type casting
-array=[231,12,34,45,65,77,886]
-print("LENGHT: ",len(array))
-print(" elements is ",array[0:2])
+array=[231,12,34,45,65,77,886]#list
+print("LENGHT: ",len(array))#list
+print(" elements  of second index is ",array[1:2])
 print("Your ID is",x)
 print('It is now ',type(z)) 
 print(l1,l2,l3)
