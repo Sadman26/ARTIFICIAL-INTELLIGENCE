6968bb04f39a45f2b1e721c94e0a6c97124619ef
Sadman-123 <19201103123@cse.bubt.edu.bd>
2022-08-31 14:03:33 +0600

    compared

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 while True:
+    def compare(x,y,z):
+        if x>y and x>z:
+            print(x,'is greatest than ',y,' and ',z)
+        elif y>x and y>z:
+            print(y,'is greatest than ',x,' and ',z)
+        else:
+            print(z,'is greatest than ',x,' and ',y)
     x=int(input("Enter X: "))
     y=int(input('Enter Y: '))
     z=int(input('Enter Z: '))
-    if x>y and x>z:
-        print(x,'is greatest')
-    elif y>x and y>z:
-        print(y,'is greatest')
-    else:
-        print(z,'is greatest')
+    compare(x,y,z)
+    
