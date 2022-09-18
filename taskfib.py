a4e7574f9d7c67f6e90db572e54ef4af55d5d726
Sadman-123 <19201103123@cse.bubt.edu.bd>
2022-09-19 00:17:09 +0600

    fib -> fibonacci

--- taskfib.py
+++ taskfib.py
@@ -1,8 +1,8 @@
-def fib(n):
+def fibonacci(n):
     if n <= 1:
         return n
     else:
-        return(fib(n-1) + fib(n-2))
+        return(fibonacci(n-1) + fibonacci(n-2))
 limit=int(input('Enter Limit: '))
 for i in range(limit):
-    print(fib(i),end=' ')
+    print(fibonacci(i),end=' ')
