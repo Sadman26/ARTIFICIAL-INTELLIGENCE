791d608e7829e50fee74722ebb6d9692154ae7de
Sadman-123 <19201103123@cse.bubt.edu.bd>
2022-09-20 21:45:46 +0600

    pseducode

--- test1.py
+++ test1.py
@@ -17,3 +17,21 @@ elif salary>=20001:
     DA=salary*0.95
     gross=salary+HRA+DA
     print('Gross Salary={}'.format(gross))
+#psedocode for the above program
+#1. input salary
+#2. if salary<=10000
+#3. HRA=salary*0.2
+#4. DA=salary*0.8
+#5. gross=salary+HRA+DA
+#6. print gross
+#7. elif salary>=10001 and salary<=20000
+#8. HRA=salary*0.25
+#9. DA=salary*0.9
+#10. gross=salary+HRA+DA
+#11. print gross
+#12. elif salary>=20001
+#13. HRA=salary*0.3
+#14. DA=salary*0.95
+#15. gross=salary+HRA+DA
+#16. print gross
+
