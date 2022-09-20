a9b4f123cadbe5da8acb1636f1cc901b93457460
Sadman-123 <19201103123@cse.bubt.edu.bd>
2022-09-20 21:01:03 +0600

    pseducode

--- taskStrong.py
+++ taskStrong.py
@@ -16,3 +16,10 @@ if strong_number(num):
     print("{} [Strong Number]".format(num))
 else:
     print("{} [Weak Number]".format(num))
+#pseduo code for this program
+# 1. define a function to calculate factorial
+# 2. define a function to calculate strong number
+# 3. take input from user
+# 4. use if else statement to print strong number
+# 5. print the result
+# 6. exit
--- taskpattern.py
+++ taskpattern.py
@@ -4,3 +4,9 @@ def pattern_make(x,y):
             print(j,end='')
         print()
 pattern_make(1,6)
+#pseduo code for this program
+# 1. define a function to make pattern
+# 2. take input from user
+# 3. use for loop to print pattern
+# 4. print the result
+# 5. exit
